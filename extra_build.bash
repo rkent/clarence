12144720526289614f7a3a42eec9d0b513005ae5
R. Kent James <kent@caspia.com>
2025-08-24 11:32:07 -0700

    Docker mods so that audio works

--- extra_build.bash
+++ extra_build.bash
@@ -1,1 +1,5 @@
-export EXTRA_BUILD_RUN="groupadd -g $(getent group i2c | cut -d: -f3) i2c -U $(whoami) ; groupadd -g $(getent group gpio | cut -d: -f3) gpio -U $(whoami)"
+export EXTRA_BUILD_RUN="
+groupadd -g $(getent group i2c | cut -d: -f3) i2c -U $(whoami)
+groupadd -g $(getent group gpio | cut -d: -f3) gpio -U $(whoami)
+usermod -aG audio $(whoami)
+"
